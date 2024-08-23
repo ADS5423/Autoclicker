e88e07c9c1b4e4dbb703e11052611ca29b01edff
ADS5423 <138416304+ADS5423@users.noreply.github.com>
2024-08-23 08:48:44 -1100

    Update Left_AutoClicker.py

--- Left_AutoClicker.py
+++ Left_AutoClicker.py
@@ -5,9 +5,6 @@ from pynput.keyboard import Listener, KeyCode
 TOGGLE_KEY = KeyCode(char='r')
 activated = False
 
-def no_work():
-    print("Why you see this?")
-
 def on_press(key):
     global activated
     if key == TOGGLE_KEY:
@@ -18,17 +15,16 @@ def doClick():
     global activated
     while True:
         if activated:
-            print("On!")
             variations = random.choice([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2])
             Controller().click(Button.left, variations)
             delay = random.uniform(0.05, 0.0709)
-            print(delay)
+            print("on" + delay)
             time.sleep(delay)
         else:
-            print("Off!?")
+            print("Off")
             pass
 
-print("working!")
+print("working")
 threading.Thread(target = doClick).start()
 listner = Listener(on_press=on_press)
 listner.start()
